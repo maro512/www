830e4214e4e2bcb39db025f842be5a35b815b873
maro512 <ool.maro@gmail.com>
2017-05-10 22:53:09 +0200

    Dodane formularze

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.article_list, name='article_list'),
     url(r'^article/(?P<pk>[0-9]+)/$', views.article_detail, name='article_detail'),
+    url(r'^article/new/$', views.article_new, name='article_new'),
+    url(r'^article/(?P<pk>[0-9]+)/edit/$', views.article_edit, name='article_edit'),
+    url(r'^article/(?P<pk>[0-9]+)/new_comment/$', views.comment_new, name='comment_new'),
 ]
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,14 +1,56 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Article
-from .models import Comment
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
 
 
 def article_list(request):
-    articles_list = Article.objects.filter(is_published=True).order_by('date')
+    articles_list = Article.objects.filter(is_published=True).order_by('-date')
     return render(request, 'newspaper/article_list.html', {'article_list': articles_list})
 
 
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
-    comments = Comment.objects.filter(article=pk).order_by('date')
+    comments = Comment.objects.filter(article=pk).order_by('-date')
     return render(request, 'newspaper/article_detail.html', {'article': article, 'comments': comments})
+
+
+def article_new(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'newspaper/article_edit.html', {'form': form})
+
+
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'newspaper/article_edit.html', {'form': form})
+
+
+def comment_new(request, pk):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        article = get_object_or_404(Article, pk=pk)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.article = article
+            comment.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'newspaper/comment_edit.html', {'form': form})
