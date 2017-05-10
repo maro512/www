2b421441a5d0b7eee2c588346e94d0b5040efe95
maro512 <ool.maro@gmail.com>
2017-05-10 14:23:50 +0200

    Szczegóły artykułu

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
+from .models import Comment
 
 
 def article_list(request):
-    articles_list = Article.objects.all().filter(is_published=True)
+    articles_list = Article.objects.filter(is_published=True).order_by('date')
     return render(request, 'newspaper/article_list.html', {'article_list': articles_list})
+
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    comments = Comment.objects.filter(article=pk).order_by('date')
+    return render(request, 'newspaper/article_detail.html', {'article': article, 'comments': comments})
