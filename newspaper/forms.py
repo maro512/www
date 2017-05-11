16b1a92189bfc2c8996af8695dcb386a4446e9e3
bvshu <jan.buszewicz@gmail.com>
2017-05-11 13:25:30 +0200

    Odrobina estetyki2

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -13,3 +13,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}),
+        }
